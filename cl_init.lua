738173b3148e9ee16c4acc59ce1dffe4cb2f7dc9
samuelds <samuelds.pro@gmail.com>
2017-06-11 14:32:41 +0200

    Update exemple

--- cl_init.lua
+++ cl_init.lua
@@ -1,6 +1,6 @@
 -- @Date:   2017-06-11T11:53:02+02:00
 -- @Project: FiveM Tools
--- @Last modified time: 2017-06-11T13:38:07+02:00
+-- @Last modified time: 2017-06-11T14:32:12+02:00
 -- @License: GNU General Public License v3.0
 
 local FirstJoinProper = false
@@ -21,7 +21,7 @@ Citizen.CreateThread(function()
       -- Press F1
       if IsControlJustPressed(1, 288) then
         if not exports.ft_menuBuilder:IsOpened() and GetLastInputMethod(2) then
-          exports.ft_menuBuilder:Open("menuOne")
+          exports.ft_menuBuilder:Open("ft_menuOne")
         else
           exports.ft_menuBuilder:Close()
         end
--- config.lua
+++ config.lua
@@ -1,11 +1,11 @@
 -- @Date:   2017-06-11T12:15:17+02:00
 -- @Project: FiveM Tools
--- @Last modified time: 2017-06-11T14:06:36+02:00
+-- @Last modified time: 2017-06-11T14:32:01+02:00
 -- @License: GNU General Public License v3.0
 
 menu = {
 
-  menuOne = {
+  ft_menuOne = {
 
     settings = {
       title = "Menu title",
@@ -16,14 +16,14 @@ menu = {
       { text = "Callback", callback = callbackExemple, data = { text = "text" } },
       { text = "Client Event", eventClient = "ft_menuExemple:ClExemple", data = { text = "text" } },
       { text = "Server Event", eventServer = "ft_menuExemple:SvExemple", data = { text = "text" } },
-      { text = "Open menu", menu = "menuTow" },
+      { text = "Open menu", menu = "ft_menuTow" },
       { text = "Open dynamic menu", callback = dynamicMenu },
       { text = "Close menu", close = true },
     },
 
   },
 
-  menuTow = {
+  ft_menuTow = {
 
     settings = {
       menuTitle = "Sub menu",
