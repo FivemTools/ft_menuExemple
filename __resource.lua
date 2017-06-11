6d644f9413f584c69b9a0a78d00e10be7754ad70
samuelds <samuelds.pro@gmail.com>
2017-06-11 20:29:18 +0200

    Update

--- __resource.lua
+++ __resource.lua
@@ -2,7 +2,7 @@
 -- @Date:   2017-06-08T23:28:39+02:00
 -- @Project: FiveM Tools
 -- @Last modified by:
--- @Last modified time: 2017-06-11T19:02:46+02:00
+-- @Last modified time: 2017-06-11T20:11:34+02:00
 -- @License: GNU General Public License v3.0
 
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
